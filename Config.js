b5dbfe6f043f32d47624566131d60a146ec856e3
AMAL AJI <93529273+Cyber-Dome@users.noreply.github.com>
2024-06-27 11:06:38 +0530

    Update Config.js

--- Config.js
+++ Config.js
@@ -3,24 +3,24 @@ const chalk = require('chalk');
 require('dotenv').config();
 
 //contact details
-global.ownernumber = [ process.env.OWNER_NUMBER || "919931122319"],
-global.ownername = process.env.OWNER_NAME || "💙𓆩‎✘AʏᴜSʜ✘𓆪_✰",
-global.ytname = "YT: AyushBotz.inc"
-global.socialm = "GitHub: Ayushpandey023"
+global.ownernumber = [ process.env.OWNER_NUMBER || "919895457440"],
+global.ownername = process.env.OWNER_NAME || "💙𓆩‎ᴀᴍᴀʟ ᴀᴊɪ✘𓆪_✰",
+global.ytname = "YT: Domebytes"
+global.socialm = "GitHub: Cyber-Dome"
 global.location = "Indian"
 
-global.botname = process.env.BOTNAME || "𝐌𝐀𝐑𝐈𝐀-𝐌𝐃",//name of the bot
+global.botname = process.env.BOTNAME || "ᴄʏʙᴇʀᴅᴏᴍᴇ",//name of the bot
 
 //sticker details
-global.stickername = process.env.STICKER || "💙𓆩‎✘AʏᴜSʜ✘𓆪_✰",
+global.stickername = process.env.STICKER || "💙𓆩‎✘‎ᴀᴍᴀʟ ᴀᴊɪ✘𓆪_✰",
 global.packname = 'Sticker By'
-global.author = process.env.AUTHOR || "💙𓆩‎✘AʏᴜSʜ✘𓆪_✰",
+global.author = process.env.AUTHOR || "💙𓆩‎✘‎ᴀᴍᴀʟ ᴀᴊɪ✘𓆪_✰",
 //console view/theme
-global.themeemoji = '🧩'
-global.wm = "Ayush botz inc."
+global.themeemoji = '👾'
+global.wm = "Amal botz inc."
 
 //theme link
-global.link = 'https://whatsapp.com/channel/0029VaImo5ZG3R3qjKOdyr1I'
+global.link = 'https://chat.whatsapp.com/FeYIOmXspRhDMkSPnkYx58'
 
 //custom prefix
 global.prefa = process.env.PREFIX || ".",
